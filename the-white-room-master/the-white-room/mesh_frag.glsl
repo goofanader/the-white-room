5d2e1d8b4f5400528258caee8486e018df98502e
goofanader@gmail.com <goofanader@gmail.com@1e1b0f51-0cf4-a5e2-d9d8-1430ba6ae172>
2013-02-16 05:19:05 +0000

    trying to commit to google code.

--- the-white-room-master/the-white-room/mesh_frag.glsl
+++ the-white-room-master/the-white-room/mesh_frag.glsl
@@ -10,19 +10,26 @@ varying vec3 vThePosition;
 
 void main() {
     //gl_FragColor = vec4(uColor, 1.0);
-  vec3 specL;
+  vec3 specL = uColor;
   vec3 V;
   V.x = uCamTrans.x - vThePosition.x; //Camera at (0, 0, 0)?
   V.y = uCamTrans.y - vThePosition.y;
   V.z = uCamTrans.z - vThePosition.z;
   V = normalize(V);
   /*if (dot(vNormal, vLightDir) < 0.0)
+    specL = vec3(0.0);
   else {*/
-      vec3 R = -vLightDir + 2.0 * max(dot(normalize(vNormal), vLightDir), 0.0)* normalize(vNormal);
+      vec3 R = -vLightDir + 2.0 * max(dot(vNormal, vLightDir), 0.0) * vNormal;
       //vec3 R = -vLightDir + 2.0 * dot(vNormal, vLightDir) * vNormal;
       //R = normalize(R);
-      float VdotR = clamp(pow(dot(V, R), uShininess), 0.0, 1.0);
-      specL = uColor * uSpecStrength * VdotR * uLightColor;
+      float VdotR = max(pow(dot(V, R), uShininess), 0.0);
+      specL.x *= VdotR;
+      specL.y *= VdotR;
+      specL.z *= VdotR;
+      specL.x *= uLightColor.x;
+      specL.y *= uLightColor.y;
+      specL.z *= uLightColor.z;
+      specL *= uSpecStrength;
   //}
 
   vec3 diffL = uColor;
@@ -39,23 +46,10 @@ void main() {
   ambL.y *= 0.1 * uColor.y * uLightColor.y;
   ambL.z *= 0.1 * uColor.z * uLightColor.z;
 
-  ambL = vec3(0.75, 0.75, 0.75);
+  ambL = vec3(0.5, 0.5, 0.5);
   vec3 finColor = diffL + specL + ambL;
   //vec3 finColor = uLightColor;
   gl_FragColor = vec4(finColor.r, finColor.g, finColor.b, 1.0);
-  //vec3 finColor = diffL + ambL;
-//  if (useNormalAsColor == 1)
-    //gl_FragColor = vec4(vNormal, 1.0);
-//  else
-    //gl_FragColor = vec4(vLightDir, 1.0);
-    //gl_FragColor = vec4(specL, 1.0);
-    //gl_FragColor = vec4(ambL.xyz, 1.0);
-    //gl_FragColor = vec4(uLightColor, 1.0);
-    /*if (uLightColor.z > 1.0)// || uColor.z > 1.0 || uLightColor.z < 0.0 || uLightColor.z > 1.0)
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    else
-        gl_FragColor = vec4(0.0, 0.0, uLightColor.z * uColor.z, 1.0);*/
-
 
     if (gl_FragCoord.x >= 397.0 && gl_FragCoord.x <= 403.0 &&
         gl_FragCoord.y >= 297.0 && gl_FragCoord.y <= 303.0) {
